75f30ab28ca343e2b80a391f5bb985568be6847b
archie294 <128092484+archie294@users.noreply.github.com>
2026-03-26 17:59:13 +0000

    force rebuild with correct base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',   // ⭐ CHANGE THIS (THIS IS THE FIX)
+  base: '/OBA-Unblocked-Games-v2/',
 })
